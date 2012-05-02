40be2f9297ee480b9666b98a705b05d6f1d22019
Valentine Zavadsky <saicheg@gmail.com>
2012-05-02 19:30:48 +0300

    add some more objects for scheduling

---
+++ lib/exact_target_sdk/delivery_profile_source_address_type_enum.rb
@@ -0,0 +1,6 @@
+module ExactTargetSDK
+module DeliveryProfileSourceAddressTypeEnum
+  CUSTOM_PRIVATE_IP_ADDRESS = "CustomPrivateIPAddress"
+  DEFAULT_PRIVATE_IP_ADDRESS = "DefaultPrivateIPAddress"
+end
+end
--- lib/exact_target_sdk/send_definition_list.rb
+++ lib/exact_target_sdk/send_definition_list.rb
@@ -1,12 +1,8 @@
 module ExactTargetSDK
 class SendDefinitionList < APIObject
 
-  property 'DataSourceTypeID', :required => true
+  property 'DataSourceTypeID'
   property 'List'
 
-  validates 'DataSourceTypeID', :inclusion => { :allow_nil => false,
-                                                   :in => %w( List CustomObject DomainExclusion SalesForceCampaign FilterDefinition OptOutList ) }
-
 end
-
 end
