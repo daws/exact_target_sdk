8dfc87ddf1e97fb9b909c2b3d395c32b09575ae4
Valentine Zavadsky <saicheg@gmail.com>
2012-05-08 09:24:30 +0300

    add some fields to data_extension

--- lib/exact_target_sdk/data_extension.rb
+++ lib/exact_target_sdk/data_extension.rb
@@ -5,5 +5,11 @@ class DataExtension < APIObject
   array_property 'Fields', :nest_children => true
   property 'Name'
 
+  # Indicates whether you can use a data extension as part of an audience for a message send.
+  property 'IsSendable'
+
+  # Indicates whether a sendable data extension can be used within tests sends for a message.
+  property 'IsTestable'
+
 end
 end
