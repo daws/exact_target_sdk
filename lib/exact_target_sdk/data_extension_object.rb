ab8e7d73c9a687f99f8c5e027820c61eca1202d7
Valentine Zavadsky <saicheg@gmail.com>
2012-05-17 22:54:40 +0300

    Add couple fields to DataExtensionObject

--- lib/exact_target_sdk/data_extension_object.rb
+++ lib/exact_target_sdk/data_extension_object.rb
@@ -2,7 +2,9 @@ module ExactTargetSDK
 class DataExtensionObject < APIObject
 
   property 'CustomerKey'
+  property 'SubscriberKey'
   array_property 'Properties', :nest_children => true
+  array_property 'Keys', :nest_children => true
 
 end
 end
