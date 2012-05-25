ff54ab1d5afccd8510becf03f84ca936eab34647
Valentine Zavadsky <saicheg@gmail.com>
2012-05-25 12:42:04 +0300

    add objects for SpamAssasin integration

---
+++ lib/exact_target_sdk/content_validation.rb
@@ -0,0 +1,14 @@
+module ExactTargetSDK
+class ContentValidation < APIObject
+
+  # Default email address for object. Indicates if subscriber information can be used for email sends.
+  property 'Email'
+
+  # Specifies the validation action to run on content. Valid values include SpamAssassinValidation.
+  property 'ValidationAction'
+
+  # Indicates subscribers associated with an object.
+  array_property 'Subscribers'
+
+end
+end
